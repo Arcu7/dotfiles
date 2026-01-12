3c516dda9be36335c286c3be8425d363501e2c17
Arcu7 <rifaldoagustus@tutanota.com>
2026-01-13 01:18:58 +0700

    update nvim

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,14 +1,14 @@
 return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy",
-  opts = {
-    default = {
-      -- file and directory options
-      dir_path = "media", ---@type string | fun(): string
-    },
-  },
-  keys = {
-    -- suggested keymap
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
+  -- "HakonHarnes/img-clip.nvim",
+  -- event = "VeryLazy",
+  -- opts = {
+  --   default = {
+  --     -- file and directory options
+  --     dir_path = "media", ---@type string | fun(): string
+  --   },
+  -- },
+  -- keys = {
+  --   -- suggested keymap
+  --   { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  -- },
 }
