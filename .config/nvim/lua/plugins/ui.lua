e29f7d2d2dec694b550c2b0337c756fa498b036a
Arcu7 <rifaldoagustus@tutanota.com>
2026-05-04 19:33:11 +0700

    fix: temp remove dropbar.nvim (and remove unused plugins)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,34 +1,22 @@
 return {
-  {
-    "Bekaboo/dropbar.nvim",
-    -- optional, but required for fuzzy finder support
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-    },
-    config = function()
-      local dropbar_api = require("dropbar.api")
-      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-      vim.api.nvim_set_hl(0, "DropBarText", { fg = "#ffffff" })
-    end,
-  },
+  -- Disable because some function used by this plugin is deprecated in Neovim 0.13
+  -- Will fix when the plugin is updated
+  -- See here:
+  -- https://github.com/Bekaboo/dropbar.nvim/issues/279
+  -- https://github.com/Bekaboo/dropbar.nvim/pull/280
   -- {
-  --   "rachartier/tiny-code-action.nvim",
+  --   "Bekaboo/dropbar.nvim",
+  --   -- optional, but required for fuzzy finder support
   --   dependencies = {
-  --     { "nvim-lua/plenary.nvim" },
-  --     {
-  --       "folke/snacks.nvim",
-  --       opts = {
-  --         terminal = {},
-  --       },
-  --     },
+  --     "nvim-telescope/telescope-fzf-native.nvim",
+  --     build = "make",
   --   },
-  --   event = "LspAttach",
-  --   opts = {
-  --     backend = "delta",
-  --     picker = "buffer",
-  --   },
+  --   config = function()
+  --     local dropbar_api = require("dropbar.api")
+  --     vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+  --     vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+  --     vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+  --     vim.api.nvim_set_hl(0, "DropBarText", { fg = "#ffffff" })
+  --   end,
   -- },
 }
